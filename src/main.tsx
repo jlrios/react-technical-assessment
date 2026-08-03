2482b8c72c464496f28a2bb182893c9af06bfcb0
JLRD <rdjl5791@gmail.com>
2026-08-03 13:55:28 -0600

    React technical assessment

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+import { PrimeReactProvider } from "primereact/api";
+import "primeicons/primeicons.css";
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
+const primeConfig = {
+  ripple: true
+}
 
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <PrimeReactProvider value={primeConfig}>
+        <App />
+      </PrimeReactProvider>
+    </Provider>
+  </StrictMode>,
+);
